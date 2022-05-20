76415ea9ea560f3e05e16ad70cc946424f15b78e
Rita Mwaikinda <srmwaikinda@gmail.com>
2022-05-20 03:05:16 -0400

    express server 1.0

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const PORT = 8080;
+
+app.use(express.json());
 
 app.listen(PORT, () =>
 	console.log(`we're up and running on http://localhost:${PORT}`)
@@ -9,5 +12,18 @@ app.get("/homepage", (req, res) => {
 	res.status(200).send({
 		pagename: "testing api",
 		message: "more in development 😎",
+	});
+});
+
+app.post("/homepage/signup/:username", (req, res) => {
+	const { username } = req.params;
+	const { name } = req.body;
+
+	if (!name) {
+		res.status(412).send({ message: "Please enter your name" });
+	}
+
+	res.status(200).send({
+		message: `Hi ${name}! Your new username is ${username}. To change this, please go to account settings.`,
 	});
 });
